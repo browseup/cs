2b01134c759f728bc80ebf5a06ef5f3b9e8cd8bb
browseup <an@browseup.ru>
2016-08-19 23:04:38 +0300

    УРРА!!

---
+++ src/main/java/io/khasang/cs/AppController.java
@@ -0,0 +1,25 @@
+package io.khasang.cs;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+//    @Autowired
+//    Message message;
+
+    @RequestMapping("/")
+    public String hello(Model model) {
+//        model.addAttribute("hello", message.getHello());
+        return "hello";
+    }
+
+    @RequestMapping("/confidential/hello")
+    public String message(Model model){
+        model.addAttribute("message", "How you receive access to this sercure page!?");
+        return "message";
+    }
+
+}
